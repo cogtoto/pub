37ecba4ec585f1ab0a8e40ec429db41f168e76d5
cogtoto <cognetvincent@gmail.com>
2022-03-13 19:16:48 +0100

    popo
    Signed-off-by: cogtoto <cognetvincent@gmail.com>

--- COQ/miniML.v
+++ COQ/miniML.v
@@ -6,7 +6,7 @@ Open Scope list_scope.
 Import ListNotations. 
 
 Section miniML.
-(** représentation des termes *)
+(* représentation des termes *)
 
 Inductive terme :=
 | Vrai : terme
@@ -14,16 +14,18 @@ Inductive terme :=
 | Var : string -> terme
 | Nb : nat -> terme
 | Plus : terme -> terme -> terme
+| Moins : terme -> terme -> terme
 | Et : terme -> terme -> terme 
 | Si : terme -> terme -> terme -> terme
-| Tete : list terme -> terme
-| Liste : list terme -> terme
-| Cons : terme -> list terme -> terme
-| Reste : list terme ->  terme
 | Lambda: string -> terme -> terme
 | Fermeture : string -> terme -> list (string*terme) -> terme
-| App : terme -> terme -> terme
+| App : terme -> terme -> terme (* App M N *)
+| Let : string -> terme -> terme -> terme (* let x N M *)
+| Letrec : string -> terme -> terme -> terme (* letrec x N M *)
 .
+
+Coercion Var : string >-> terme.
+Coercion Nb : nat >-> terme.
 
 Inductive valeur : terme -> Prop := 
 | Nb_v : forall n:nat, valeur (Nb n)
@@ -31,11 +33,9 @@ Inductive valeur : terme -> Prop :=
 | Faux_va:  valeur Faux
 .
 
-(** représentation de l'environnement [\Gamma] *)
+(* représentation de l'environnement  *)
 Definition env := list (string * terme) .
-
-Definition env0 : env := [("x",Nb 1); ("y", Nb 2); ("v", Vrai)].
-Print env0.
+Definition env0 : env := [("z",Nb 1); ("y", Nb 2); ("v", Vrai)].
 
 Fixpoint lookup (ident:string) (e:env):terme :=
     match e with
@@ -43,32 +43,61 @@ Fixpoint lookup (ident:string) (e:env):terme :=
     | (x,y)::reste => if x =? ident then y else lookup ident reste
     end.
 
-
 (** la sémantique *)
 Inductive sem (e:env): terme -> terme -> Prop  :=
 | r_vrai : sem e Vrai Vrai
 | r_faux : sem e Faux Faux
 | r_nb : forall n:nat, sem e (Nb n) (Nb n)
 | r_var : forall x:string, sem e (Var x) (lookup x e)
+
 | r1_et : forall (t1 t2:terme) , sem e t1 Faux  -> sem e (Et t1 t2) Faux
-| r2_et : forall (t1 t2 v2:terme) , sem e t1 Vrai  -> sem e t2 v2 -> valeur v2 -> sem e (Et t1 t2) v2
-| r_plus : forall (t1 t2:terme) (n1 n2:nat) , sem e t1 (Nb n1) -> sem e t2 (Nb n2) -> sem e (Plus t1 t2) (Nb (n1+n2))
-| r_si_vrai : forall (t1 t2 v2 t3:terme), sem e t1 Vrai -> sem e t2 v2 -> valeur v2 -> sem e (Si t1 t2 t3) v2
-| r_si_faux : forall (t1 t2 t3 v3:terme), sem e t1 Faux -> sem e t3 v3 -> valeur v3 -> sem e (Si t1 t2 t3) v3
-(* fonctions sur liste*)
-| r_cons : forall (v1:terme) (l2: list terme), valeur v1 -> sem e (Cons v1 l2) (Liste (v1::l2))
-(* lambda expression *)
+| r2_et : forall (t1 t2 v2:terme),
+    sem e t1 Vrai  ->
+    sem e t2 v2 -> 
+    valeur v2 ->
+    sem e (Et t1 t2) v2
+
+| r_plus : forall (t1 t2:terme) (n1 n2 n3:nat) ,
+    sem e t1 (Nb n1) -> 
+    sem e t2 (Nb n2) -> 
+    n3 = n2 + n1 ->
+    sem e (Plus t1 t2) (Nb n3)
+
+| r_moins : forall (t1 t2:terme) (n1 n2 n3:nat) ,
+    sem e t1 (Nb n1) -> 
+    sem e t2 (Nb n2) -> 
+    n3 = n1 - n2 ->
+    sem e (Moins t1 t2) (Nb n3)
+
+| r_si_vrai : forall (t1 t2 v2 t3:terme), sem e t1 Vrai -> sem e t2 v2 ->
+              valeur v2 -> sem e (Si t1 t2 t3) v2
+| r_si_faux : forall (t1 t2 t3 v3:terme), sem e t1 Faux -> sem e t3 v3 ->
+              valeur v3 -> sem e (Si t1 t2 t3) v3
+
+(* lambda *)
 | r_lambda : forall (x:string) (corps:terme), sem e (Lambda x corps) (Fermeture x corps e)
-(* application *)
-| r_app : forall (f corps t1 v1 w:terme) (x:string) (env1:env),
-    sem e f (Fermeture x corps env1) -> 
-    sem e t1 v1 ->
-    sem  ([(x,v1)] ++ env1) corps w ->
-    sem e (App f t1) w
+
+(* application  App M N *)
+| r_app : forall (M M' N v v' :terme) (x:string) (e':env),
+    sem e M (Fermeture x M' e') -> 
+    sem e N v ->
+    sem  ([(x,v)] ++ e') M' v' ->
+    sem e (App M N) v'
+
+(* let x N M *)
+| r_let : forall (N M v v' :terme) (x:string) ,
+    sem e N v  ->
+    sem ([(x,v)] ++ e) M v' ->
+    sem e (Let x N M) v'
+
+(* letrec x N M *)
+| r_letrec : forall (N M v v' :terme) (x:string) ,
+    sem ([(x,v)] ++ e) N v  ->
+    sem ([(x,v)] ++ e) M v' ->
+    sem e (Letrec x N M) v'
 .
 
-
-Lemma  test1 : sem env0 (Var "x") (Nb 1) .
+Lemma  test1 : sem env0 (Var "z") (Nb 1) .
 apply r_var. Qed.
 
 Hint Constructors sem valeur .
@@ -78,39 +107,40 @@ apply r2_et.
 auto. auto. auto.
 Qed.
 
-Lemma test2 : sem env0 (Et Vrai (Et (Var "z") Vrai)) Faux.
+Lemma test2 : sem env0 (Et Vrai (Et (Var "v") Faux)) Faux.
 apply r2_et.
-auto. apply r1_et.
-apply r_var. auto.
+auto. apply r2_et.
+apply r_var. auto. auto. auto.
 Qed.
 
 Lemma test4 : sem env0 (Et Vrai (Et (Var "v") Vrai)) Vrai.
 apply r2_et. repeat auto. apply r2_et. apply r_var. auto. auto. auto.
 Qed.
 
-Lemma test5 : sem env0 (Plus (Nb 2) (Nb 5)) (Nb (2+5)).
-apply r_plus.  auto. auto.
+Lemma test5 : sem env0 (Plus (Nb 2) (Nb 5)) (Nb 7).
+simpl.
+eapply r_plus.  auto. auto. simpl. auto.
 Qed.
 
 Lemma test6 : sem env0 (Si Vrai (Nb 3) (Nb 5)) (Nb 3).
 apply r_si_vrai. auto. auto. auto.
 Qed.
 
-Lemma t7 : sem env0 (Cons (Nb 5) [(Nb 6); (Nb 7)]) (Liste [(Nb 5); (Nb 6); (Nb 7)]).
-auto.
+Definition id : terme := Lambda "x" (Var "x").
+Lemma t11 : sem env0 (App id (Nb 5)) (Nb 5).
+unfold id.
+eapply r_app.
+apply r_lambda.
+apply r_nb.
+simpl.
+apply r_var.
 Qed.
-
-Lemma t8 : exists t:terme, sem env0 (Cons (Nb 5) [(Nb 6); (Nb 7)]) t.
-eauto .
-Qed.
-
-Lemma t9 : {t:terme| sem env0 (Cons (Nb 5) [(Nb 6); (Nb 7)]) t}.
-eauto.
-Qed.
-
-Print t9.
 
 Definition succ : terme := Lambda "x" (Plus (Var "x") (Nb 1)).
+
+Lemma t12 : sem (("x", Nb 5)::env0) (Var "x") (Nb 5).
+apply r_var.
+Qed.
 
 Lemma t10 : sem env0 (App succ (Nb 5)) (Nb 6).
 unfold succ.
@@ -118,8 +148,52 @@ eapply r_app.
 eapply r_lambda.
 eapply r_nb.
 simpl.
-assert (sem [("x", Nb 1); ("y", Nb 2); ("v", Vrai); ("x", Nb 5)] (Var "x") (Nb 1)).
-eapply r_var.
-apply (r_plus 
+eapply r_plus.
+unfold env0.
 
-(*  r_plus : forall (t1 t2:terme) (n1 n2:nat) , sem e t1 (Nb n1) -> sem e t2 (Nb n2) -> sem e (Plus t1 t2) (Nb (n1+n2)) *)
+assert (sem [("x", Nb 5); ("z", Nb 1); ("y", Nb 2); ("v", Vrai)] (Var "x") (Nb 5)).
+ apply r_var.
+ eauto.  
+apply r_nb.
+simpl. 
+auto.
+Qed.
+
+Lemma t13 : sem env0 (Moins (Nb 6) (Nb 5)) (Nb 1).
+eapply r_moins.
+apply r_nb.
+apply r_nb.
+simpl.
+reflexivity.
+Qed.
+
+Lemma t14 : sem env0 (Let "x" (Nb 5) (Var "x")) (Nb 5).
+eapply r_let.
+apply r_nb.
+apply r_var.
+Qed.
+
+Lemma t15: sem env0 (Letrec "mon_plus" (Lambda "x" (Plus (Var "x") (Nb 1))) (App (Var "mon_plus") (Nb 5))) (Nb 6).
+eapply r_letrec.
+eapply r_lambda.
+Admitted.
+
+
+Fixpoint mon_plus (n m:nat) {struct n}  : nat :=
+match n with
+| 0 => m 
+| S n => S (mon_plus n m)
+end.
+
+Compute mon_plus 3 4.
+
+Compute (fun x=> x+1) 3.
+Compute (let x := 3 in x+1).
+
+Notation "e |= A :~ B" := (sem e A B) (at level 69).
+
+Lemma t16 : env0 |= "y" :~ 2.
+unfold env0.
+apply r_var.
+Qed.
+
